be6df9050df46b434d31891353633b18d78496f7
ManeeshDev <manishprashangamage23@gmail.com>
2022-07-31 06:06:57 +0530

    Add files from 2019 static website

---
+++ js/form-popup.js
@@ -0,0 +1,121 @@
+/* popup form _js */
+
+
+function openForm() {
+	document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+	document.getElementById("myForm").style.display = "none";
+}
+// -----------------------------------------------------------------------------------		
+		
+function openForm1() {
+	document.getElementById("myForm1").style.display = "block";
+}
+
+function closeForm1() {
+	document.getElementById("myForm1").style.display = "none";
+}
+// -----------------------------------------------------------------------------------
+
+function openForm2() {
+	document.getElementById("myForm2").style.display = "block";
+}
+
+function closeForm2() {
+	document.getElementById("myForm2").style.display = "none";
+}
+// -----------------------------------------------------------------------------------
+
+function openForm3() {
+	document.getElementById("myForm3").style.display = "block";
+}
+
+function closeForm3() {
+	document.getElementById("myForm3").style.display = "none";
+}
+// -----------------------------------------------------------------------------------
+
+function openForm4() {
+	document.getElementById("myForm4").style.display = "block";
+}
+
+function closeForm4() {
+	document.getElementById("myForm4").style.display = "none";
+}
+// -----------------------------------------------------------------------------------		
+		
+function openForm5() {
+	document.getElementById("myForm5").style.display = "block";
+}
+
+function closeForm5() {
+	document.getElementById("myForm5").style.display = "none";
+}
+// -----------------------------------------------------------------------------------
+
+function openForm6() {
+	document.getElementById("myForm6").style.display = "block";
+}
+
+function closeForm6() {
+	document.getElementById("myForm6").style.display = "none";
+}
+// -----------------------------------------------------------------------------------
+
+function openForm7() {
+	document.getElementById("myForm7").style.display = "block";
+}
+
+function closeForm7() {
+	document.getElementById("myForm7").style.display = "none";
+}
+// -----------------------------------------------------------------------------------
+
+function openForm8() {
+	document.getElementById("myForm8").style.display = "block";
+}
+
+function closeForm8() {
+	document.getElementById("myForm8").style.display = "none";
+}
+// -----------------------------------------------------------------------------------
+
+function openForm9() {
+	document.getElementById("myForm9").style.display = "block";
+}
+
+function closeForm9() {
+	document.getElementById("myForm9").style.display = "none";
+}
+// -----------------------------------------------------------------------------------
+
+function openForm10() {
+	document.getElementById("myForm10").style.display = "block";
+}
+
+function closeForm10() {
+	document.getElementById("myForm10").style.display = "none";
+}
+// -----------------------------------------------------------------------------------
+function openForm11() {
+	document.getElementById("myForm11").style.display = "block";
+}
+
+function closeForm11() {
+	document.getElementById("myForm11").style.display = "none";
+}
+// -----------------------------------------------------------------------------------
+function openForm12() {
+	document.getElementById("myForm12").style.display = "block";
+}
+
+function closeForm12() {
+	document.getElementById("myForm12").style.display = "none";
+}
+// -----------------------------------------------------------------------------------
+
+
+
+
